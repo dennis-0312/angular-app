704fc1e1c81f43e8fe91a9c763b7a514258ad248
dennis-0312 <dennisfernandez.issh@gmail.com>
2021-08-25 12:20:30 -0500

    first

---
+++ angular/my-first-app/src/environments/test.js
@@ -0,0 +1,55 @@
+/**
+ *@NApiVersion 2.x
+ *@NScriptType Restlet
+ */
+define(['N/search'], function(search) {
+
+    function _get(context) {
+        
+    }
+
+    function _post(context) {
+        
+    }
+
+    function _put(context) {
+        
+    }
+
+    function _delete(context) {
+        
+    }
+
+    return {
+        get: _get,
+        post: _post,
+        put: _put,
+        delete: _delete
+    }
+});
+
+
+/**
+ *@NApiVersion 2.x
+ *@NScriptType UserEventScript
+ */
+define([], function() {
+
+    function beforeLoad(context) {
+        
+    }
+
+    function beforeSubmit(context) {
+        
+    }
+
+    function afterSubmit(context) {
+        
+    }
+
+    return {
+        beforeLoad: beforeLoad,
+        beforeSubmit: beforeSubmit,
+        afterSubmit: afterSubmit
+    }
+});
